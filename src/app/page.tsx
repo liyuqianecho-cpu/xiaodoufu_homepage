47ac7bf3479e9bb6720641b3470aefae3e51a989
小豆腐home <xiaodoufuhome@xiaodoufuhomedeMac-mini.local>
2026-03-24 09:57:07 +0800

    🐱 换成工作时的头像！吃零食看电脑！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,7 +208,7 @@ export default function Home() {
           >
             <div className="absolute inset-0 bg-[#f4ddc5] rounded-full blur-3xl opacity-50 animate-pulse" />
             <Image
-              src="/images/site-assets/xiaodoufu-avatar-cute.png"
+              src="/images/site-assets/xiaodoufu-avatar-working.png"
               alt="小豆腐"
               width={288}
               height={288}
